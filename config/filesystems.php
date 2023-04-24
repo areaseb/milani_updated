f66c2ec4130c03c68ccf3bdbcb83b3c5a0067b33
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2023-04-24 12:36:07 +0200

    Removed old order export

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,18 +61,6 @@ return [
             'root' => storage_path('app/tmp'),
             'throw' => false,
         ],
-
-        'export' => [
-            'driver' => 'ftp',
-            'host' => env('EXPORT_FTP_HOST'),
-            'username' => env('EXPORT_FTP_USERNAME'),
-            'password' => env('EXPORT_FTP_PASSWORD'),
-            'port' => env('EXPORT_FTP_PORT', 21),
-            'root' => env('EXPORT_FTP_ROOT', '/'),
-            'ssl' => env('EXPORT_FTP_SSL', false),
-            'timeout' => env('EXPORT_FTP_TIMEOUT', 30)
-        ]
-
     ],
 
     /*
