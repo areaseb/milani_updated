939157d395a4d226adacb16ec1481be193282806
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2023-04-24 15:20:12 +0200

    Scheduled order export

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\BeezupStartAutoImportCommand;
+use App\Console\Commands\ExportOrderCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -17,6 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(BeezupStartAutoImportCommand::class)->dailyAt('01:00');
+        $schedule->command(ExportOrderCommand::class)->everyTenMinutes();
     }
 
     /**
