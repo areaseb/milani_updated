5a5a406e2259e1a01d08779ae8abb397c03b49b4
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2024-02-23 11:07:32 +0100

    Fixed layout newsletter form

--- platform/themes/milani/partials/shortcodes/newsletter-form.blade.php
+++ platform/themes/milani/partials/shortcodes/newsletter-form.blade.php
@@ -3,13 +3,13 @@
         <div class="row align-items-center">
             <div class="col-lg-7 mb-md-3 mb-lg-0">
                 <div class="row align-items-center">
-                    <div class="col flex-horizontal-center">
+                    <div class="col flex-horizontal-center title">
                         <img class="icon-email" src="{{ Theme::asset()->url('images/icons/icon-email.svg') }}" alt="icon">
                         <h4 class="font-size-20 mb-0 ml-3">{!! BaseHelper::clean($title) !!}</h4>
                     </div>
-                    <div class="col my-4 my-md-0">
+                    {{-- <div class="col my-4 my-md-0">
                         <h5 class="font-size-15 ml-4 mb-0">{!! BaseHelper::clean($description) !!}</h5>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
             <div class="col-lg-5">
@@ -18,13 +18,13 @@
                     @csrf
                     <div class="form-subcriber d-flex wow fadeIn animated ">
                         <input type="email" name="email" class="form-control bg-white font-small" placeholder="{{ __('Enter your email') }}">
-                        <button class="btn bg-dark text-white" type="submit" disabled>{{ __('Subscribe') }}</button>
+                        <button class="btn bg-dark text-white desktop-button" type="submit" disabled>{{ __('Subscribe') }}</button>
                     </div>
 
                     <div class="chek-form">
                         <div class="custome-checkbox">
                             <input class="form-check-input" type="checkbox" name="privacy_policy" id="privacy_policy" required>
-                            <label class="form-check-label" for="privacy_policy"><span>{!! __('I agree to the :[1]privacy policy:[/1]', ['[1]' => '<a href="/privacy-policy">', '[/1]' => '<a>']) !!}</span></label>
+                            <label class="form-check-label" for="privacy_policy"><span>{!! __('I agree to the :[1]privacy policy:[/1]', ['[1]' => '<a href="/privacy-policy">', '[/1]' => '</a>']) !!}</span></label>
                         </div>
                     </div>
 
@@ -33,6 +33,8 @@
                             {!! Captcha::display() !!}
                         </div>
                     @endif
+
+                    <button class="btn bg-dark text-white mobile-button" type="submit" disabled>{{ __('Subscribe') }}</button>
                 </form>
                 <!-- End Subscribe Form -->
             </div>
