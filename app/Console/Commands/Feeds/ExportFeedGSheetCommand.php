2016eb768270b78e811386e64deace99d17e5f86
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2024-03-13 12:03:42 +0100

    Refactored commands name

--- app/Console/Commands/Feeds/ExportFeedGSheetCommand.php
+++ app/Console/Commands/Feeds/ExportFeedGSheetCommand.php
@@ -2,32 +2,31 @@
 
 namespace App\Console\Commands\Feeds;
 
-use App\Services\Feed\GSheet;
-use App\Services\Feeds\SeoPortingFeedExporter;
+use App\Services\Feeds\GSheetFeedExporter;
 use Illuminate\Console\Command;
 
-class FeedSeoPortingCommand extends Command
+class ExportFeedGSheetCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'feed:seoporting:export';
+    protected $signature = 'feed:gsheet:export';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Export Seo Porting feed';
+    protected $description = 'Export GSheet feed';
 
     /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle(SeoPortingFeedExporter $exporter)
+    public function handle(GSheetFeedExporter $exporter)
     {
         $exporter->export();
 
