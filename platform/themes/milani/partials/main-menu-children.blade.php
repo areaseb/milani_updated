8d1eeac1c06ce6f5285287cbc75a7081bf98b30a
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2024-03-13 16:23:23 +0100

    Fixed menu

--- platform/themes/milani/partials/main-menu-children.blade.php
+++ platform/themes/milani/partials/main-menu-children.blade.php
@@ -1,4 +1,4 @@
-<div class="sub-menu" x-data="{ item: 0 }">
+<div class="sub-menu" x-data="{ main: 0, sub: -1 }">
     <div class="first-col">
         <div>
             <ul>
@@ -6,8 +6,8 @@
                     $index = 0;
                 @endphp
                 @foreach ($menu_nodes as $key => $row)
-                    <li>
-                        <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}" x-on:mouseover="item = {{ $index }}" x-bind:class="{ 'active': item == {{ $index }}}">
+                    <li x-on:mouseover="main = {{ $index }}, sub = -1" x-bind:class="{ 'active': main == {{ $index }}}">
+                        <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}">
                             {{ $row->title }}
                             @if ($row->has_child)
                                 @if ($row->parent_id) <i class="fa fa-chevron-right"></i> @else <i class="fa fa-chevron-down"></i> @endif
@@ -26,13 +26,20 @@
             @endphp
             @foreach ($menu_nodes as $key => $row)
                 @if ($row->has_child)
-                    <ul x-show="item == {{ $index }}" class="sub-sub-menu">
+                    <ul x-show="main == {{ $index }}" class="sub-sub-menu">
+                        @php
+                            $sub = 0;
+                        @endphp
                         @foreach ($row->child as $key => $row)
-                            <li>
+                            <li x-on:mouseenter="sub = {{ $sub }}" x-on:mouseleave="sub = -1">
                                 <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}">
                                     {{ $row->title }}
                                 </a>
                             </li>
+
+                            @php
+                                $sub++;
+                            @endphp
                         @endforeach
                     </ul>
                 @endif
@@ -49,11 +56,29 @@
             $index = 0;
         @endphp
         @foreach ($menu_nodes as $key => $row)
-            <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}" x-show="item == {{ $index }}">
+            <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}" x-show="main == {{ $index }} && sub == -1">
                 @if ($iconImage = $row->getMetadata('icon_image', true))
                     <img src="{{ RvMedia::getImageUrl($iconImage) }}" alt="{{ $row->title }}"/>
                 @endif
             </a>
+
+            @if ($row->has_child)
+                @php
+                    $sub = 0;
+                @endphp
+                @foreach ($row->child as $key => $row)
+                    <a href="{{ url($row->url) }}" @if ($row->active) class="active" @endif target="{{ $row->target }}" x-show="main == {{ $index }} && sub == {{ $sub }}">
+                        @if ($iconImage = $row->getMetadata('icon_image', true))
+                            <img src="{{ RvMedia::getImageUrl($iconImage) }}" alt="{{ $row->title }}"/>
+                        @endif
+                    </a>
+
+                    @php
+                        $sub++;
+                    @endphp
+                @endforeach
+            @endif
+
             @php
                 $index++;
             @endphp
