643e7b0104195a9db46e07763c8a4ccffff21968
Areaseb <108459623+DavideMoscheni@users.noreply.github.com>
2024-04-19 13:15:59 +0200

    Update header.blade.php
    Added Iubenda JS

--- platform/themes/milani/partials/header.blade.php
+++ platform/themes/milani/partials/header.blade.php
@@ -53,6 +53,14 @@
             }
             $headerStyle = ($headerStyle && in_array($headerStyle, array_keys(get_layout_header_styles()))) ? $headerStyle : '';
         @endphp
+
+        <script type="text/javascript">
+		var _iub = _iub || [];
+		_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferencesButtonDisplay":"bottom-left","perPurposeConsent":true,"siteId":2335001,"whitelabel":false,"cookiePolicyId":64090603,"lang":"it", "banner":{ "acceptButtonDisplay":true,"backgroundOverlay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"bottom","showTitle":false }};
+		</script>
+		<script type="text/javascript" src="https://cs.iubenda.com/autoblocking/2335001.js"></script>
+		<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+
     </head>
     <body @if (BaseHelper::siteLanguageDirection() == 'rtl') dir="rtl" @endif class="@if (BaseHelper::siteLanguageDirection() == 'rtl') rtl @endif header_full_true wowy-template css_scrollbar lazy_icons btnt4_style_2 zoom_tp_2 css_scrollbar template-index wowy_toolbar_true hover_img2 swatch_style_rounded swatch_list_size_small label_style_rounded wrapper_full_width header_full_true header_sticky_true hide_scrolld_true des_header_3 h_banner_true top_bar_true prs_bordered_grid_1 search_pos_canvas lazyload @if (Theme::get('bodyClass')) {{ Theme::get('bodyClass') }} @endif">
         {!! apply_filters(THEME_FRONT_BODY, null) !!}
