674219a4cbe212f506c5867898dc8cba78a4406a
Areaseb <108459623+DavideMoscheni@users.noreply.github.com>
2024-05-07 19:39:30 +0200

    Update master.blade.php
    added GTM

--- platform/plugins/ecommerce/resources/views/orders/master.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/master.blade.php
@@ -5,7 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title> @yield('title', __('Checkout')) </title>
-
+	
+	<!-- Google Tag Manager -->
+		<script>
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-MPBC629');
+		</script>
+	<!-- End Google Tag Manager -->
+	
     @if (theme_option('favicon'))
         <link rel="shortcut icon" href="{{ RvMedia::getImageUrl(theme_option('favicon')) }}">
     @endif
@@ -42,6 +52,13 @@
     @stack('header')
 </head>
 <body class="checkout-page" @if (BaseHelper::siteLanguageDirection() == 'rtl') dir="rtl" @endif>
+	
+	<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPBC629" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
     {!! apply_filters('ecommerce_checkout_body', null) !!}
     <div class="checkout-content-wrap">
         <div class="container">
