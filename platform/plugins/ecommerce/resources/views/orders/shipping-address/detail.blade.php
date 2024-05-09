7a4636b36f98bc4863198b423e2a59e60829e458
Areaseb <108459623+DavideMoscheni@users.noreply.github.com>
2024-05-09 17:33:20 +0200

    Update detail.blade.php
    added vat, sdi, pec

--- platform/plugins/ecommerce/resources/views/orders/shipping-address/detail.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/shipping-address/detail.blade.php
@@ -15,18 +15,30 @@
         <div>{{ $address->address }}</div>
     @endif
     @if ($address->city)
-        <div>{{ $address->city_name }}</div>
-    @endif
-    @if ($address->state)
-        <div>{{ $address->state_name }}</div>
-    @endif
+        <div>
+        	@if (EcommerceHelper::isZipCodeEnabled() && $address->zip_code)
+		        {{ $address->zip_code }} 
+		    @endif
+		    {{ $address->city_name }} 
+		    @if ($address->state)
+		        ({{ $address->state_name }})
+		    @endif
+		</div>
+    @endif    
     @if ($address->country_name)
         <div>{{ $address->country_name }}</div>
     @endif
-    @if (EcommerceHelper::isZipCodeEnabled() && $address->zip_code)
-        <div>{{ $address->zip_code }}</div>
+    @if ($address->vat)
+        <div><b>P.IVA / C.F.</b> {{ $address->vat }}</div>
     @endif
-    <div>
+    @if ($address->sdi)
+        <div><b>SDI</b> {{ $address->sdi }}</div>
+    @endif
+    @if ($address->pec)
+        <div><b>PEC</b> {{ $address->pec }}</div>
+    @endif
+    
+    <div style="text-align: right;">
         <a target="_blank" class="hover-underline" href="https://maps.google.com/?q={{ $address->address }}, {{ $address->city_name }}, {{ $address->state_name }}, {{ $address->country_name }}@if (EcommerceHelper::isZipCodeEnabled()), {{ $address->zip_code }} @endif">{{ trans('plugins/ecommerce::order.see_on_maps') }}</a>
     </div>
 </li>
